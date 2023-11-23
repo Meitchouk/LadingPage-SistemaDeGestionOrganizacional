59464468ab520ac7ca179a7c2329256e359c1e82
Meitchouk <lanzamarat@gmail.com>
2023-11-23 13:58:50 -0600

    Se suben cambios añadiendo mas informacion del proyecto

--- js/carousel.js
+++ js/carousel.js
@@ -1,36 +1,38 @@
 document.addEventListener("DOMContentLoaded", function () {
     const carousel = document.querySelector(".carousel");
-    const prevButton = document.getElementById("prev-slide");
-    const nextButton = document.getElementById("next-slide");
-    const imageFolder = "/img"; // Ruta de la carpeta de imágenes
+    const imageNames = [
+        "1- Desagregación del Sistema",
+        "2- Diagrama de Contexto",
+        "3- Caja Negra",
+        "4- CAJA BLANCA - Subsistema de Epidemiología",
+        "5- CAJA BLANCA - Subsistema de Administración",
+        "6- CAJA BLANCA - Subsistema de Enfermería",
+        "7- CAJA BLANCA - Subsistema de Dirección Médica",
+        "8- Modelo de Diamante",
+        "9- Árbol de objetivos",
+        "10- Árbol de problemas",
+        "11- Matriz de Optimización de objetivos versus alternativas",
+        "12- Diagrama sin RC",
+        "13- Ruta Crítica",
+        "14- Diagrama de GANTT"
+    ];
+    const maxImagesToShow = imageNames.length;
     let currentIndex = 0;
 
     // Función para cargar las imágenes de la carpeta dinámicamente
     function loadImages() {
-        fetch(imageFolder) // Hacer una solicitud HTTP para obtener la lista de archivos en la carpeta
-            .then((response) => response.text())
-            .then((html) => {
-                const parser = new DOMParser();
-                const doc = parser.parseFromString(html, "text/html");
-                const links = Array.from(doc.querySelectorAll("a"));
-                const imageFiles = links
-                    .filter((link) => /\.(jpg|jpeg|png)$/i.test(link.href)) // Filtrar solo archivos JPG o PNG
-                    .map((link) => link.href);
+        imageNames.forEach((imageName, index) => {
+            const img = document.createElement("img");
+            img.src = `img/${index + 1}.png`;
+            img.alt = imageName;
+            const slide = document.createElement("div");
+            slide.className = "slide";
+            slide.appendChild(img);
+            carousel.appendChild(slide);
+        });
 
-                // Cargar las imágenes dinámicamente en el carrusel
-                imageFiles.forEach((file) => {
-                    const img = document.createElement("img");
-                    img.src = file;
-                    img.alt = decodeURIComponent(file.split("/").pop().split(".")[0]); // Decodificar la URL y obtener el nombre del archivo sin extensión
-                    const slide = document.createElement("div"); // Crea un contenedor div para cada imagen
-                    slide.className = "slide";
-                    slide.appendChild(img);
-                    carousel.appendChild(slide);
-                });
-
-                showCurrentSlide(); // Mostrar la primera imagen
-                updateCarouselTitle(currentIndex); // Actualizar el título después de cargar las imágenes
-            });
+        showCurrentSlide(); // Mostrar la primera imagen
+        updateCarouselTitle(currentIndex);
     }
 
     // Función para mostrar la imagen actual
@@ -38,45 +40,55 @@ document.addEventListener("DOMContentLoaded", function () {
         const slides = carousel.querySelectorAll(".slide");
         slides.forEach((slide, index) => {
             if (index === currentIndex) {
-                slide.style.transform = "translateX(0%)"; // Muestra la imagen actual
+                slide.style.display = "block"; // Muestra la imagen actual
             } else {
-                const offset = (index - currentIndex) * 100; // Calcula el desplazamiento
-                slide.style.transform = `translateX(${offset}%)`; // Aplica el desplazamiento
+                slide.style.display = "none"; // Oculta las demás imágenes
             }
         });
     }
 
-    // Avanzar al siguiente slide
-    nextButton.addEventListener("click", function () {
+    // Cambiar automáticamente las imágenes cada 30 segundos
+    setInterval(() => {
         currentIndex++;
-        if (currentIndex >= carousel.childElementCount) {
-            currentIndex = 0; // Vuelve al primer slide al llegar al final
+        if (currentIndex >= maxImagesToShow) {
+            currentIndex = 0;
         }
         showCurrentSlide();
         updateCarouselTitle(currentIndex);
-    });
-
-    // Retroceder al slide anterior
-    prevButton.addEventListener("click", function () {
-        currentIndex--;
-        if (currentIndex < 0) {
-            currentIndex = carousel.childElementCount - 1; // Vuelve al último slide al llegar al principio
-        }
-        showCurrentSlide();
-        updateCarouselTitle(currentIndex);
-    });
-
-    // Cargar automáticamente las imágenes al cargar la página
-    loadImages();
+    }, 30000);
 
     // Función para actualizar el título
     function updateCarouselTitle(index) {
-        const currentImage = carousel.querySelector(`.slide:nth-child(${index + 1}) img`);
-        const imageName = currentImage ? currentImage.alt : "";
         const carouselTitle = document.getElementById("carousel-title");
-        carouselTitle.textContent = imageName;
+        carouselTitle.textContent = imageNames[index];
     }
 
-    // Llamada inicial para mostrar el título de la primera imagen
-    //updateCarouselTitle(0); // Comentamos esta llamada inicial
+    // Cargar automáticamente las imágenes al cargar la página
+    loadImages();
+    
+    function prevSlide() {
+        currentIndex--;
+        if (currentIndex < 0) {
+            currentIndex = maxImagesToShow - 1;
+        }
+        showCurrentSlide();
+        updateCarouselTitle(currentIndex);
+    }
+    
+    function nextSlide() {
+        currentIndex++;
+        if (currentIndex >= maxImagesToShow) {
+            currentIndex = 0;
+        }
+        showCurrentSlide();
+        updateCarouselTitle(currentIndex);
+    }
+    
+    // Eventos para los botones externos
+    const prevButton = document.getElementById("prevButton");
+    const nextButton = document.getElementById("nextButton");
+    
+    prevButton.addEventListener("click", prevSlide);
+    nextButton.addEventListener("click", nextSlide);
 });
+
